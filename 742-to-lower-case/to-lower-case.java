f0d1a968cccffb959e86de714f9e2296bece10a0
bandisreesatya <bandisreesatya@gmail.com>
2026-06-15 15:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (33.20%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,16 +1,5 @@
 class Solution {
     public String toLowerCase(String s) {
-        int n=s.length();
-        StringBuilder str=new StringBuilder();
-        for(int i=0;i<n;i++){
-            char ch=s.charAt(i);
-            if(Character.isUpperCase(ch)){
-                str.append(Character.toLowerCase(ch));
-            }
-            else{
-                str.append(ch);
-            }
-        }
-        return str.toString();
+        return s.toLowerCase();
     }
 }
