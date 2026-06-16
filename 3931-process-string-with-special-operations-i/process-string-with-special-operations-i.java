315d843cf706e03360ac5c70cf904d6bd1781434
bandisreesatya <bandisreesatya@gmail.com>
2026-06-16 13:15:11 +0530

    Time: 3 ms (100.00%) | Memory: 54.9 MB (80.88%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -4,19 +4,19 @@ class Solution {
         StringBuilder str=new StringBuilder();
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
-            if(ch>='a' && ch<='z'){
-                str.append(ch);
+            if(ch=='#'){
+                str.append(str);
             }
             else if(ch=='*'){
                 if(str.length()!=0){
                     str.deleteCharAt(str.length()-1);
-            }
+                }
             }
             else if(ch=='%'){
                 str.reverse();
             }
             else{
-                str.append(str);
+                str.append(ch);
             }
     }
     return str.toString();
