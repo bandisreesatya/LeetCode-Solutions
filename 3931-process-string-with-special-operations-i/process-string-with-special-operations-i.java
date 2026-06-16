959e1183d564ab6db5674dc5a76efc3635c65839
bandisreesatya <bandisreesatya@gmail.com>
2026-06-16 13:13:09 +0530

    Time: 4 ms (76.47%) | Memory: 55 MB (75.00%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String processStr(String s) {
+        int n=s.length();
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch>='a' && ch<='z'){
+                str.append(ch);
+            }
+            else if(ch=='*'){
+                if(str.length()!=0){
+                    str.deleteCharAt(str.length()-1);
+            }
+            }
+            else if(ch=='%'){
+                str.reverse();
+            }
+            else{
+                str.append(str);
+            }
+    }
+    return str.toString();
+}
+}
