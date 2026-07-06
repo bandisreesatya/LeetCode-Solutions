3a529a9eb5959a46cfabdcfa0d1fc824c1c291cd
bandisreesatya <bandisreesatya@gmail.com>
2026-07-06 20:48:05 +0530

    Time: 10 ms (25.94%) | Memory: 42.1 MB (51.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,13 +1,11 @@
 class Solution {
-    static int[] arr=new int[31];
-    static{
-        arr[0]=0;
-        arr[1]=1;
-        for(int i=2;i<31;i++){
-            arr[i]=arr[i-1]+arr[i-2];
-        }
-    }
     public int fib(int n) {
-        return arr[n];
+        if(n==0 ){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
     }
 }
