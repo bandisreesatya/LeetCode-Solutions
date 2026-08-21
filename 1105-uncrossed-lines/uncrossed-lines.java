873b8f337dfffac444ecbc6d5ef53603e1350823
Bandi Sree Satya <bandisreesatya@gmail.com>
2026-08-21 15:30:20 +0530

    Time: 5 ms (99.18%) | Memory: 45.8 MB (60.14%) - LeetSync

---
+++ 1105-uncrossed-lines/uncrossed-lines.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int[][] dp=new int[n1+1][n2+1];
+        for(int i=n1-1;i>=0;i--){
+            for(int j=n2-1;j>=0;j--){
+                if(nums1[i]==nums2[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i][j+1],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
