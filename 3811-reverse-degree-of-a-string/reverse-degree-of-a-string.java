bfa69a43ddc6f31d92d3391727982a26fa746509
Bandi Sree Satya <bandisreesatya@gmail.com>
2026-09-20 13:16:58 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (34.29%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseDegree(String s) {
+        int n = s.length();
+        int sum = 0;
+
+        for (int  i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            sum += (i + 1) * ('z' - ch + 1);
+        }
+
+        return sum;
+    }
+}
